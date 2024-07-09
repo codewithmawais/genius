42df6f67779e177b9585c2a478474d23281d547d
codewithmawais <connectwithmawais>
2024-07-10 04:13:11 +0500

    Implemented Code UI and API.

---
+++ app/(dashboard)/(routes)/code/page.tsx
@@ -0,0 +1,149 @@
+"use client";
+
+import * as z from "zod";
+import axios from "axios";
+import { Code } from "lucide-react";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+// import { toast } from "react-hot-toast";
+import ReactMarkdown from "react-markdown";
+import { useRouter } from "next/navigation";
+import OpenAI from 'openai';
+
+import Heading from "@/components/heading";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form";
+import { cn } from "@/lib/utils";
+import { Empty } from "@/components/empty";
+import { Loader } from "@/components/loader";
+
+import { formSchema } from "./constants";
+import { UserAvatar } from "@/components/user-avatar";
+import { BotAvatar } from "@/components/bot-avatar";
+
+const CodePage = () => {
+    const router = useRouter();
+    const [messages, setMessages] = useState<OpenAI.ChatCompletionMessageParam[]>([
+        // Below data is only for test purpose.
+        { role: "user", content: "What's your name?" },
+        { role: "assistant", content: 
+        "```javascript\
+import React, { useState } from \'react\';\
+// ToggleButton component\
+const ToggleButton = () => {\
+  // useState hook to manage the toggle state\
+  const [isToggled, setIsToggled] = useState(false);\
+  // Function to handle the button click and toggle the state\
+  const handleToggle = () => {\
+    setIsToggled(!isToggled);\
+  };\
+  return (\
+    <button onClick={handleToggle}>\
+      {isToggled ? \'ON\' : \'OFF\'}\
+    </button>\
+  );\
+};\
+export default ToggleButton;\
+" }
+    ]);
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            prompt: ""
+        }
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const userMessage: OpenAI.ChatCompletionMessageParam = { role: "user", content: values.prompt };
+            const newMessages = [...messages, userMessage];
+            
+            const response = await axios.post('/api/code', { messages: newMessages });
+            setMessages((current) => [...current, userMessage, response.data]);
+            
+            form.reset();
+        } catch (error: any) {
+            // toast.error("Something went wrong.");
+            console.log("Something went wrong.", error);
+        } finally {
+            router.refresh();
+        }
+    }
+
+    return (
+        <div>
+            <Heading
+                title="Code Generation"
+                description="Generate code using descriptive text."
+                icon={Code}
+                iconColor="text-green-700"
+                bgColor="bg-green-700/10"
+            />
+            <div className="px-4 lg:px-8">
+                <div>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+                        >
+                            <FormField
+                                control={form.control}
+                                name="prompt"
+                                render={({ field }) => (
+                                    <FormItem className="col-span-12 lg:col-span-10">
+                                        <FormControl className="m-0 p-0">
+                                            <Input
+                                                className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                                                disabled={isLoading}
+                                                placeholder="Simple toggle button using react hooks." {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )}
+                            />
+                            <Button className="col-span-12 lg:col-span-2 w-full" type="submit" disabled={isLoading}>Generate</Button>
+                        </form>
+                    </Form>
+                </div>
+                <div className="space-y-4 mt-4">
+                    {isLoading && (
+                        <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+                            <Loader />
+                        </div>
+                    )}
+                    {messages.length === 0 && !isLoading && (
+                        <Empty label="No code generated." />
+                    )}
+                    <div className="flex flex-col-reverse gap-y-4">
+                        {messages.map((message, index) => (
+                            <div
+                                key={index}
+                                className={cn("p-8 w-full flex items-start gap-x-8 rounded-lg", message.role === "user" ? "bg-white border border-black/10" : "bg-muted")}
+                            >
+                                {message.role === "user" ? <UserAvatar /> : <BotAvatar />}
+                                <ReactMarkdown components={{
+                                    pre: ({ node, ...props }) => (
+                                        <div className="overflow-auto w-full my-2 bg-black/10 p-2 rounded-lg">
+                                            <pre {...props} />
+                                        </div>
+                                    ),
+                                    code: ({ node, ...props }) => (
+                                        <code className="bg-black/10 rounded-lg p-1" {...props} />
+                                    )
+                                    }} className="text-sm overflow-hidden leading-7">
+                                    {typeof message.content === 'string' ? message.content : ''}
+                                </ReactMarkdown>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default CodePage;
